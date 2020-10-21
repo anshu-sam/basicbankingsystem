68e16dc0cc25ac096e67ed37533ef140aa61392e
Anshu Sam <59832097+anshu-sam@users.noreply.github.com>
2020-10-21 14:14:20 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Banking System</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<center>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Belleza&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Lora:wght@700&display=swap');
+body{
+background-image:url("https://images.unsplash.com/photo-1543269865-0a740d43b90c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80");
+background-size:1368px 700px;
+}
+p{
+font-size:40px;
+text-align:center;
+margin:150px 150px 150px 150px;
+font-family: 'Belleza', sans-serif;
+}
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   height:35px;
+   background-color:black;
+   box-shadow: inset 0 6.2em 0 rgba(0, 0, 0, 0);
+   z-index:1;
+}
+</style>
+<body>
+<div class="container">    
+<p font-size="30px">A NEW BANKING EXPERIENCE IS HERE!
+<br>Save Easily. Bank Beautifully.  </p>
+<button type="button" class="btn btn-primary" style="width:400px; height:50px;"><a href="list.php">
+  &#160;&#160;&#160;&#160;&#160;<b style="color:white; font-size:30px;">VIEW CUSTOMERS</b>&#160;&#160;&#160;&#160;&#160;</a></button>   
+</div>
+</body>
+</center>
+
+</html>
+<?php
+include("foot.php");
+?>
