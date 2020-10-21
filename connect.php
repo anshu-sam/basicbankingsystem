8cb093bf6f425590da1903cbe2bac0c6a19c8b13
Anshu Sam <59832097+anshu-sam@users.noreply.github.com>
2020-10-21 14:11:12 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,5 @@
+<?php
+	$db_name="epiz_27010899_sparks";
+	$cn=mysqli_connect("sql305.epizy.com","epiz_27010899","9eGdzB5w4m3kMC","epiz_27010899_sparks" ) or die("Could not Connect My Sql");
+	mysqli_select_db ($cn ,$db_name ) or die("Could not connect")
+?>
