20d913a768c6d36e66b071d551c3ea3568199274
Anshu Sam <59832097+anshu-sam@users.noreply.github.com>
2020-10-21 14:15:00 +0530

    Create list.php

---
+++ list.php
@@ -0,0 +1,259 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/style.css">
+	<met http-equiv="refresh" content="1"> 
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css\default.css"><link rel="stylesheet" href="css/fontawesome.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Rakkas' rel='stylesheet'>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+<style>
+.header
+{
+	box-shadow: inset 0 6.2em 0 rgba(0, 0, 0, 0.2);
+	background-color:#4d94ff;
+    height:20px;
+}
+a:link {
+  color: white;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:visited {
+  color: white;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+a:hover {
+  color: red;
+  background-color: transparent;
+  text-decoration: underline;
+}
+
+a:active {
+  color: yellow;
+  background-color: transparent;
+  text-decoration: underline;
+}
+</style>
+</head>
+<body>
+
+<div class="header" style="background-color:black" >
+ <a href="home.php"></a> &#160;  &#160; <a href="index.php">HOME
+ <div class="name">
+</div></a>
+</div>
+
+<div style="padding-left:20px">
+ 
+</div>
+
+</body>
+</html>
+<html>
+	<style>
+		.art1 b
+		{
+			font-size:30px;
+			color: black;
+			font-size:40px;
+			font-family: "Times New Roman", Times, serif;
+		}
+	</style>
+<body style="background-color:#e6ecff;">
+<center>
+<body>
+<div class="container">
+	 <div>
+					<div class="art1">
+							<center><b><u>LIST OF CUSTOMERS</u></b></center>							
+						</div>
+							</div>
+				</div>
+	<br><br>
+		<table border = 4>
+			<th width=100 height=40>&#160;&#160;&#160;&#160;&#160;&#160;<b style="color:#0000A0; font-size:15px;">Name</b></th>
+			<th width=400 height=40>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<b style="color:#0000A0; font-size:15px;">E-mail</th>
+			<th width=200 height=40>&#160;&#160;&#160;&#160;&#160;&#160;<b style="color:#0000A0; font-size:15px;">Current Balance</th>
+			<th width=150 height=40>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<b style="color:#0000A0; font-size:15px;"><b>VIEW</b></th>
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Anshu</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>anshusam123@hotmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>671</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid1"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Harshitha</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>harshi12@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>518</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid10"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Shwetha</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>shwetha12@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>200</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid11"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Jaanvi</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>jaanvi@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>800</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid12"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Arunima</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>arunima123@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>6183</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid13"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Navneet</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>navneet18@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>500</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid14"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Abhishek</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>abhishek09@outlook.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>4300</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid15"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Chethan</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>chethan12@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>4256</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid2"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Meghana</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>meghana02@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>332</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid3"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Suraj</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>suraj1234@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>1000</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid4"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Daniel</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>daniel12@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>1900</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid5"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Gabriel</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>gabriel123@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>5650</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid6"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Emily</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>emilyinparis@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>3200</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid7"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Rohan</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>rohansam@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>3600</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid8"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+	
+	<tr>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>Phoebe</b></td>		
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>phoebe12@gmail.com</b></td>
+		<td>&#160;&#160;&#160;&#160;&#160;&#160;<b>952</b></td>
+		<td> &#160;&#160;&#160;&#160;&#160;&#160; <a href="credit.php?userid=uid9"><button class="btn btn-primary" style="width:100px; height:30px; margin-top:2px; margin-bottom:2px; margin-left:-5px;">VIEW</button></td>
+
+	</tr>
+	</center>
+</body>
+	
+		</table>
+       
+</html>
+<?php
+include("foot.php");
+?>
